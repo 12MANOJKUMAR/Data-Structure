2f79b7ce2d9eb41ebd5129fa3b42d1a524184ac6
Manoj Kumar <124412880+12MANOJKUMAR@users.noreply.github.com>
2025-06-24 06:07:38 +0530

    Time: 7 ms (73.95%) | Memory: 22.8 MB (73.50%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool check(const long long &mid, vector<int>& p,int h){
+       long long ans=0;
+        for(auto it: p){
+            ans+=(it+mid-1)/mid;
+        }
+        return ans<=h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        // set invarient 
+        long long low=0, high=1e12;
+        while(high>low+1){
+            long long mid = low+(high-low)/2;
+            // check condition
+            if(check(mid,piles,h)){
+                high = mid;
+            }
+            else {
+                low=mid;
+            }
+        
+        }
+        return high;
+    }
+};

e0a8a79673e8acc8646e3440ad6c72635fd2a91e
Manoj Kumar <124412880+12MANOJKUMAR@users.noreply.github.com>
2025-06-26 08:45:43 +0530

    Time: 8 ms (84.62%) | Memory: 35 MB (34.18%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    static bool check(const int& capct, vector<int>& wt, int d) {
+    int daycnt = 1, currentwt = 0;
+        for (auto it : wt) {
+            if (it > capct) return false;
+            if (currentwt + it > capct) {
+                daycnt++;
+                currentwt = it;
+            } else {
+                currentwt += it;
+            }
+        }
+        return daycnt <= d;
+    }
+
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+
+        while (high > low + 1) {
+            int mid = low + (high - low) / 2;
+            if (check(mid, weights, days)) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+        if (check(low, weights, days)) return low;
+        return high;
+    
+            // because FFFFTTTT pattern → high is the first True
+    }
+};

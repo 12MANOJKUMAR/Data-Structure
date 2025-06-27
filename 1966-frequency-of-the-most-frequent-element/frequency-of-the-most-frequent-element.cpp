624139ad2beb12badbc8ba6bc86990c25eba611f
Manoj Kumar <124412880+12MANOJKUMAR@users.noreply.github.com>
2025-06-27 15:36:48 +0530

    Time: 47 ms (25.07%) | Memory: 105.8 MB (90.66%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    bool check(int mid,vector<int>& num, vector<long long> & pf, const int & k){
-        int n=num.size();
-        for(int i=mid-1;i<n;i++){
-            int w= i-mid+1;
-            long long total = 1LL * mid * num[i]; 
-            long long currsum = pf[i]-(w>0 ? pf[w-1]: 0);
-
-        long long reqop= total -currsum;
-        if(reqop<=k) return true;
-        }
-        return false;
-    }
     int maxFrequency(vector<int>& nums, int k) {
-       sort(nums.begin(), nums.end());
-       int l=0, r=nums.size()+1;
-       int n=nums.size();
-       vector<long long> prefix(n);
-       prefix[0] = nums[0];
-       for(int i =1; i < n; ++i)
-        prefix[i] = prefix[i - 1] + nums[i];
-
-       while(r>l+1){
-        int mid =(r+l)/2;
-        if(check(mid, nums,prefix, k)){
-            l= mid;
+         sort(nums.begin(), nums.end());
+        int left = 0;
+        int ans = 0;
+        long curr = 0;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            long target = nums[right];
+            curr += target;
+            
+            while ((right - left + 1) * target - curr > k) {
+                curr -= nums[left];
+                left++;
+            }
+            
+            ans = max(ans, right - left + 1);
         }
-        else r=mid;
-       } 
-        return l;
+        
+        return ans;
     }
 };

1c0807b8c20c34631f3432d7ffd1a4190afe23a6
Manoj Kumar <124412880+12MANOJKUMAR@users.noreply.github.com>
2025-08-20 07:53:45 +0530

    Time: 1 ms (68.55%) | Memory: 9.5 MB (94.93%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -9,24 +9,28 @@ public:
 
     string helper( const int &num){
         if(num<10) return belowten[num];
-        if(num<20) return belowtwenty[num-9];
-        if(num<100) return twodigit[num/10]+helper(num%10);
-        if(num<1000) return helper(num/100)+" Hundred"+ helper(num%100);
-
-        for(int i=3;i>=0;i--){
-            if(num>=pow(1000,i)){
-                return helper(num/(int)pow(1000,i))+special[i]+helper(num%(int)pow(1000,i));
-            }
-            
-
+        else if(num<20) return belowtwenty[num-9];
+        else if(num<100) return twodigit[num/10]+helper(num%10);
+        else if(num<1000) return helper(num/100)+" Hundred"+ helper(num%100);
+        return "";
         }
-         return "";
-    }
  
 
     string numberToWords(int num) {
         if(num==0) return "Zero";
-        string ans= helper(num).substr(1);
-        return ans;
+        string ans="";
+        int idx=0;
+        
+        while(num>0){
+            int chunk = num%1000;
+            if(chunk!=0){
+                string words = helper(chunk)+special[idx];
+                ans= words+ans; // prepend into answer...
+            }
+            num/=1000;
+            idx++;
+        }
+
+        return ans.substr(1);
     }
 };

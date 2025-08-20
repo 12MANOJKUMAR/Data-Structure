86da9ac6e47b61b89032c3015fa516272323aeb3
Manoj Kumar <124412880+12MANOJKUMAR@users.noreply.github.com>
2025-08-20 07:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (87.87%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+ 
+    // helper function to convert the integer into the enlish word;
+    string belowten[10]={""," One"," Two"," Three"," Four", " Five"," Six", " Seven", " Eight", " Nine"};
+    string belowtwenty[11]={""," Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"};
+    string twodigit[11]={"",""," Twenty"," Thirty", " Forty"," Fifty"," Sixty", " Seventy", " Eighty"," Ninety"};
+    string special[4]={"", " Thousand", " Million", " Billion"};
+
+    string helper( const int &num){
+        if(num<10) return belowten[num];
+        if(num<20) return belowtwenty[num-9];
+        if(num<100) return twodigit[num/10]+belowten[num%10];
+        if(num<1000) return helper(num/100)+" Hundred"+ helper(num%100);
+
+        for(int i=3;i>=0;i--){
+            if(num>=pow(1000,i)){
+                return helper(num/(int)pow(1000,i))+special[i]+helper(num%(int)pow(1000,i));
+            }
+            
+
+        }
+         return "";
+    }
+ 
+
+    string numberToWords(int num) {
+        if(num==0) return "Zero";
+        string ans= helper(num).substr(1);
+        return ans;
+    }
+};

095f1a5f7eec9d4fa256496431f58da21176688e
Manoj Kumar <124412880+12MANOJKUMAR@users.noreply.github.com>
2025-12-29 20:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (19.95%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-         sort(intervals.begin(), intervals.end());
-         vector<vector<int>>ans;
+        vector<vector<int>>ans;
+        int i=0, n=intervals.size();
 
-        ans.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            // overlapping
-            if(ans.back()[1]>=intervals[i][0]){
-                ans.back()[1]= max(ans.back()[1], intervals[i][1]);
-            }
-            // non-overlapping
-            else {
-                ans.push_back(intervals[i]);
-            }
-
+        while(i<n && intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
         }
-        return ans;
 
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = min( intervals[i][0],newInterval[0] );
+            newInterval[1] = max( intervals[i][1],newInterval[1] );
+            i++;
+        }
 
-        
+        ans.push_back(newInterval);
+
+        while(i<n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        return ans;    
     }
 };
